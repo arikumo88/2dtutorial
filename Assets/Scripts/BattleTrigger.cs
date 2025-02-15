64d3f0f3f222d7465ea0ffe8d34cfb5b1da450b1
arikumo88 <cloud.daikan72@gmail.com>
2025-02-15 22:24:04 +0900

    add

--- Assets/Scripts/BattleTrigger.cs
+++ Assets/Scripts/BattleTrigger.cs
@@ -4,8 +4,10 @@ public class BattleTrigger : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Trigger Entrered by: " + other.gameObject.name);
         if (other.CompareTag("Player"))
         {
+            Debug.Log("Player has entered the battle trigger");
             SceneManager.LoadScene("BattleScene");
         }
     }
