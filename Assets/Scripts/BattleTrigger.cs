65ef9376e0b8a0d5442cae360350ccfd4a61e67b
arikumo88 <cloud.daikan72@gmail.com>
2025-02-17 00:44:59 +0900

    scenechangeeffect

--- Assets/Scripts/BattleTrigger.cs
+++ Assets/Scripts/BattleTrigger.cs
@@ -1,14 +1,34 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
 public class BattleTrigger : MonoBehaviour
 {
+    public Image fadePanel;
+    public float fadeDuration = 0.3f;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Trigger Entrered by: " + other.gameObject.name);
         if (other.CompareTag("Player"))
         {
             Debug.Log("Player has entered the battle trigger");
-            SceneManager.LoadScene("BattleScene");
+            StartCoroutine(FadeToBattle());
         }
+    }
+
+    IEnumerator FadeToBattle()
+    {
+        float elapsedTime = 0f;
+        Color fadeColor = fadePanel.color;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            fadeColor.a = Mathf.Lerp(0f, 1f, elapsedTime / fadeDuration);
+            fadePanel.color = fadeColor;
+            yield return null;
+        }
+        SceneManager.LoadScene("BattleScene");
     }
 }
