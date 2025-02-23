b021b3e70bb82049f9d9adaf942d9323bdeb2ad4
arikumo88 <cloud.daikan72@gmail.com>
2025-02-23 21:32:27 +0900

    fix animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,14 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator animator;
+    private float lastMoveX;
+    private float lastMoveY;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -17,6 +22,26 @@ public class PlayerController : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical");
 
         movement = movement.normalized;
+
+        bool isMoving = movement.magnitude > 0;
+
+        Debug.Log("isMoving: " + isMoving);
+        
+        if(isMoving)
+        {
+            lastMoveX = movement.x;
+            lastMoveY = movement.y;
+        }
+
+        animator.SetFloat("MoveX", movement.x);
+        animator.SetFloat("MoveY", movement.y);
+        animator.SetBool("IsMoving", isMoving);
+
+        if(!isMoving)
+        {
+            animator.SetFloat("LastMoveX", lastMoveX);
+            animator.SetFloat("LastMoveY", lastMoveY);
+        }
     }
 
     void FixedUpdate()
